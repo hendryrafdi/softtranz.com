1d0e93385b836df0d914fd93985f6fb485b0e3ac
Hendry Abi Rafdi <hendry.rafdi@live.com>
2013-02-18 16:55:51 +0700

    new

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Software Transformator</title>
+        <link rel="stylesheet" type="css/text" href="css/style.css"/>
+        <script src="http://jqueryjs.googlecode.com/files/jquery-1.3.js" type="text/javascript"></script>  
+        <script src="js/jquery.easing.1.3.js" type="text/javascript"></script>  
+        <script src="js/animated-menu.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <div class="header">
+            <div class="logo">
+                <span class="tit">Software Transformator</span>
+            </div>
+            <div class="navbar">
+                <hr>
+                <ul>
+                    <li class="black">
+                        <p><a href="#">Home</a></p>
+                    </li>
+                    <li class="black">
+                        <p><a href="#">Member</a></p>
+                        <p class="subtext">Get in touch</p>
+                    </li>
+                    <li class="black">
+                        <p><a href="#">Schedule</a></p>
+                        <p class="subtext">Send us your stuff!</p>
+                    </li>
+                    <li class="black">
+                        <p><a href="#">Sharing</a></p>
+                        <p class="subtext">Legal things</p>
+                    </li>
+                    <li class="black">
+                        <p><a href="#">Chating</a></p>
+                        <p class="subtext">Legal things</p>
+                    </li>
+                    <li class="black">
+                        <p><a href="#">About</a></p>
+                        <p class="subtext"><a href="#">RPL</a></p>
+                        <p class="subtext"><a href="#">Programmer</a></p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="container">
+            <div class="banner">
+                <img src="images/JKT48background2.jpg" style="width: 1365px; height: 485px;"/>
+            </div>
+        </div>
+    </body>
+</html>
