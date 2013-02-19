42c1ed1aa6ff2bf7c803442bac7e4252a3243003
Hendry Abi Rafdi <hendry.rafdi@live.com>
2013-02-19 13:43:01 +0700

    update

--- member.php
+++ member.php
@@ -1,16 +1,12 @@
-<!--
-To change this template, choose Tools | Templates
-and open the template in the editor.
--->
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Member - Softranz</title>
-		<link rel="stylesheet" href="css/style.css" type="text/css">
+        <link rel="stylesheet" href="css/style.css" type="text/css">
     </head>
     <body>
-         <div class="header">
+        <div class="header">
             <div class="logo">
                 <span class="tit">Software Transformator</span>
             </div>
@@ -44,50 +40,55 @@ and open the template in the editor.
                 </ul>
             </div>
         </div>
-		<div class="container">
-			<div >
-					<font class="judul">All Member Of Software Transformator</font>
-					<img class="header-member" src="images/member/header/header.png">
-			</div>
-			<div class="row">
-				<ul>
-					<?php
-						include 'page/config.php';
-						$que = mysql_query("select *from member");
-						while($tampil = mysql_fetch_array($que)){
-						?>
-					 <li style="height: 300px; list-style: none;">
-						<a class="product" href="#">
-							<img class="thumb" src="<?php echo $tampil[foto]?>" alt="Product Image 1" />
-							<table border="0" style="margin-top: -10px;">
-								<tr>
-									<td>
-										<span>-</span>
-									</td>
-									<td class="font-nama">
-										<span><?php echo $tampil[nama_member]?></span>
-									</td>
-								</tr>
-								<tr>
-									<td>
-										<span>-</span>
-									</td>
-									<td class="font-nama">
-										<span><?php echo $tampil[quote]?></span>
-									</td>
-								</tr>
-							</table>
-						</a>
-					 </li>
-					 <?php
-						}
-					 ?>
-					 
-				</ul>
-			</div>
-		</div>
+        <div class="container">
+            <div >
+                <font class="judul">All Member Of Software Transformator</font>
+                <img class="header-member" src="images/member/header/header.png">
+            </div>
+            <div class="row">
+                <ul>
+                    <?php
+                    include 'page/config.php';
+                    $que = mysql_query("select *from member");
+                    while ($tampil = mysql_fetch_array($que)) {
+                        ?>
+                        <li style="height: 300px; list-style: none;">
+                            <a class="product" href="#">
+                                <img class="thumb" src="<?php echo $tampil[foto] ?>" alt="Product Image 1" />
+                                <table border="0" style="margin-top: -10px;">
+                                    <tr>
+                                        <td>
+                                            <span>-</span>
+                                        </td>
+                                        <td class="font-nama">
+                                            <span><?php echo $tampil[nama_member] ?></span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <span>-</span>
+                                        </td>
+                                        <td class="font-nama">
+                                            <span><?php echo $tampil[quote] ?></span>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </a>
+                        </li>
+                        <?php
+                    }
+                    ?>
+
+                </ul>
+            </div>
+        </div>
         <?php
         // put your code here
         ?>
     </body>
+    <footer>
+        <div class="kaki">
+            Copyright &copy; <a href="index.php">Software Transformator</a>. All right reserved.
+        </div>
+    </footer>
 </html>
