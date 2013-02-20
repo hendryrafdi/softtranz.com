3b1ba32641c64341a830233c81bb63d27951b88f
Cirpang <dilafadila48@yahoo.com>
2013-02-20 16:32:30 +0700

    tester

--- member.php
+++ member.php
@@ -46,40 +46,226 @@
                 <img class="header-member" src="images/member/header/header.png">
             </div>
             <div class="row">
-                <ul>
-                    <?php
-                    include 'page/config.php';
-                    $que = mysql_query("select *from member");
-                    while ($tampil = mysql_fetch_array($que)) {
-                        ?>
-                        <li style="height: 300px; list-style: none;">
-                            <a class="product" href="#">
-                                <img class="thumb" src="<?php echo $tampil[foto] ?>" alt="<?php echo $tampil[nama_member] ?>" />
-                                <table border="0" style="margin-top: -10px;">
+				<ul>
+				<?php
+						switch($_GET[id]){
+							default:
+							
+								include 'page/config.php';
+								$que = mysql_query("select *from member");
+								while ($tampil = mysql_fetch_array($que)) {
+							
+								echo"
+								<li style='height: 300px; list-style: none;'>
+									<a class='product' href='member.php?id=member&detail=$tampil[id_member]'>
+                                <img class='thumb' src='$tampil[foto]' alt='$tampil[nama_member]' />
+                                <table border='0' style='margin-top: -10px;'>
                                     <tr>
                                         <td>
-                                            <span-</span>
+                                            <span>-</span>
                                         </td>
-                                        <td class="font-nama">
-                                            <span><?php echo $tampil[nama_member] ?></span>
+                                        <td class='font-nama'>
+                                            <span>$tampil[nama_member]</span>
                                         </td>
                                     </tr>
                                     <tr>
                                         <td>
-                                            <span></span>
+                                            <span>-</span>
                                         </td>
-                                        <td class="font-nama">
-                                            <span><?php echo $tampil[quote] ?></span>
+                                        <td class='font-nama'>
+                                            <span>$tampil[quote]</span>
                                         </td>
                                     </tr>
                                 </table>
                             </a>
                         </li>
-                        <?php
-                    }
-                    ?>
-
+						";
+                    }   	
+				case'member':
+					include 'page/config.php';
+					if(isset($_GET['detail'])){
+						$que = mysql_query("select *from member where id_member='$_GET[detail]'");
+						$tampil = mysql_fetch_array($que);
+						
+						echo"
+						
+						<div class='detail'>
+							<fieldset class='border'></fieldset>
+								<div class='jarak'>
+								<a class='product'>
+									<img class='thumb_detail' src='$tampil[foto]'>
+								</a>
+								</div>
+								<fieldset class='border'>
+								<div class='table_detail'>
+									
+									<table border='0' class='table'>
+										<tr style='height:50px'>
+											<td colspan='3'>
+												<center><font face='rockwell' size='5'>Detail Member</font></center>
+											</td>
+										</tr>
+										<tr style='height: 30px'>
+											<td>
+												<font face='rockwell' size='3'>Full Name</font>
+											</td>
+											<td>
+												<font face='rockwell' size='3'>:</font>
+											</td>
+											<td>
+												<font face='rockwell' size='3'>$tampil[nama_member]</font>
+											</td>
+										</tr>
+										<tr style='height: 30px'>
+											<td>
+												<font face='rockwell' size='3'>Birthday Place</font>
+											</td>
+											<td>
+												<font face='rockwell' size='3'>:</font>
+											</td>
+											<td>
+												<font face='rockwell' size='3'>$tampil[tempat_lahir]</font>
+											</td>
+										</tr>
+										<tr style='height: 30px'>
+											<td>
+												<font face='rockwell' size='3'>Birthday Date</font>
+											</td>
+											<td>
+												<font face='rockwell' size='3'>:</font>
+											</td>
+											<td>
+												<font face='rockwell' size='3'>$tampil[tgl_lahir]</font>
+											</td>
+										</tr>
+										<tr style='height: 30px'>
+											<td>
+												<font face='rockwell' size='3'>Nickname</font>
+											</td>
+											<td>
+												<font face='rockwell' size='3'>:</font>
+											</td>
+											<td>
+												<font face='rockwell' size='3'>$tampil[nickname]</font>
+											</td>
+										</tr>
+										<tr style='height: 30px'>
+											<td>
+												<font face='rockwell' size='3'>Website</font>
+											</td>
+											<td>
+												<font face='rockwell' size='3'>:</font>
+											</td>
+											<td>
+												<font face='rockwell' size='3'>$tampil[web]</font>
+											</td>
+										</tr>
+										<tr style='height: 30px'>
+											<td>
+												<font face='rockwell' size='3'>Quote</font>
+											</td>
+											<td>
+												<font face='rockwell' size='3'>:</font>
+											</td>
+											<td>
+												<font face='rockwell' size='3'>$tampil[quote]</font>
+											</td>
+										</tr>
+									</table>
+									<div class='garis'>
+										&nbsp;
+									</div>
+									<div class='table_akun'>
+										<table border='0' class='table'>
+										<tr style='height:50px'>
+											<td colspan='3'>
+												<center><font face='rockwell' size='5'>Sosial Media</font></center>
+											</td>
+										</tr>
+										<tr style='height: 30px'>
+											<td>
+												<font face='rockwell' size='3'>Facebook</font>
+											</td>
+											<td>
+												<font face='rockwell' size='3'>:</font>
+											</td>
+											<td>
+												<font face='rockwell' size='3'>Chadana Chaiyank Cemuaa</font>
+											</td>
+										</tr>
+										<tr style='height: 30px'>
+											<td>
+												<font face='rockwell' size='3'>Twitter</font>
+											</td>
+											<td>
+												<font face='rockwell' size='3'>:</font>
+											</td>
+											<td>
+												<font face='rockwell' size='3'>@sadana_cute</font>
+											</td>
+										</tr>
+										<tr style='height: 30px'>
+											<td>
+												<font face='rockwell' size='3'>Jangkrik</font>
+											</td>
+											<td>
+												<font face='rockwell' size='3'>:</font>
+											</td>
+											<td>
+												<font face='rockwell' size='3'>Chadana</font>
+											</td>
+										</tr>
+										<tr style='height: 30px'>
+											<td>
+												<font face='rockwell' size='3'>Skype</font>
+											</td>
+											<td>
+												<font face='rockwell' size='3'>:</font>
+											</td>
+											<td>
+												<font face='rockwell' size='3'>Chadana.Cute</font>
+											</td>
+										</tr>
+										<tr style='height: 30px'>
+											<td>
+												<font face='rockwell' size='3'>Kaskus</font>
+											</td>
+											<td>
+												<font face='rockwell' size='3'>:</font>
+											</td>
+											<td>
+												<font face='rockwell' size='3'> - </font>
+											</td>
+										</tr>
+										<tr style='height: 30px'>
+											<td>
+												<font face='rockwell' size='3'>Email</font>
+											</td>
+											<td>
+												<font face='rockwell' size='3'>:</font>
+											</td>
+											<td>
+												<font face='rockwell' size='3'>SadanaCute@Manis.com</font>
+											</td>
+										</tr>
+									</table>
+									<div class='button1'>
+										<a class='css3button' href='member.php'>Back</a>
+									</div>
+									</div>
+									</fieldset>
+								</div>
+						</div>	
+						
+						";
+					}
+				}
+				?>
+				
                 </ul>
+            </div>
+			<div>
+                <img class="header-member-bawah" src="images/member/header/header.png">
             </div>
         </div>
         <?php
